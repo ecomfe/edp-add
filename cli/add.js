ca463aaa8854427d1e12dafe6c63ea8f17e68bc5
leeight <leeight@gmail.com>
2014-03-28 16:58:15 +0800

    Add cli directory

---
+++ cli/add.js
@@ -0,0 +1,79 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * lib/add.js ~ 2014/02/08 11:29:00
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ * edp add js a.js
+ * edp add html a.html
+ * edp add action a.action
+ * ...
+ **/
+
+/**
+ * 命令行配置项
+ *
+ * @inner
+ * @type {Object}
+ */
+var cli = {};
+
+/**
+ * @const
+ * @type {Array.<string>}
+ */
+cli.options = ['id:', 'type:', 'force:'];
+
+/**
+ * 命令描述信息
+ *
+ * @type {string}
+ */
+cli.description = '自动生成一些脚手架文件。';
+
+/**
+ * 模块命令行运行入口
+ *
+ * @param {Array.<string>} args
+ * @param {Object} opts
+ */
+cli.main = function ( args, opts ) {
+    require( '../index' ).start( args, opts );
+};
+
+/**
+ * 命令行配置项
+ *
+ * @type {Object}
+ */
+exports.cli = cli;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
